9e72f2f1fb1bd54a732e46f9b37cce40fc6494b0
deveshagale@gmail.com <deveshagale@gmail.com>
2025-02-05 18:33:24 +0530

    Method Class of Bank Account...

---
+++ src/Bank_Account_Management_System/Bank_Details.java
@@ -0,0 +1,20 @@
+package Bank_Account_Management_System;
+
+public class Bank_Details {
+	
+	long accNumber;
+	double deposit_money;
+	double withdraw;
+	double balance;
+	
+	public Bank_Details(long accNumber, double deposit_money, double withdraw, double balance) {
+
+		this.accNumber = accNumber;
+		this.deposit_money = deposit_money;
+		this.withdraw = withdraw;
+		this.balance = balance;
+	}
+	
+	
+
+}
