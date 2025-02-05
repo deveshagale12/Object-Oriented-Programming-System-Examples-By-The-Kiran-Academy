fc3e3e7598a43deacc7616f86b78732f0d9852af
deveshagale@gmail.com <deveshagale@gmail.com>
2025-02-05 23:20:35 +0530

    Main class is updated

--- src/Bank_Account_Management_System/Bank_Account_Management_System_Main.java
+++ src/Bank_Account_Management_System/Bank_Account_Management_System_Main.java
@@ -7,20 +7,40 @@ public class Bank_Account_Management_System_Main {
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
 		
+		
+		Bank_Details b=new Bank_Details();
+
 		do {
 			
-			System.out.println("================================================");
-			System.out.println("Select the Option ");
-			System.out.println("1. Create a Bank Account");
-			System.out.println("2. Deposit Money ");
-			System.out.println("3. Withdraw Money ");
-			System.out.println("4. Check Balance ");
-			System.out.println("================================================");
+			System.out.println("1. Create Account");
+			System.out.println("2. Deposit Amount");
+			System.out.println("3. Withdraw Amount");
+			System.out.println("4. Show Balance");
+			System.out.println("5. Exit");
 			
+
+			System.out.println("Enter your Choice : ");
+			int ch=sc.nextInt(); 
+			
+			switch(ch) {
+			
+			case 1:b.accountCreate(b.accNumber);break;
+			case 2:System.out.println("Enter the Deposit Ammount : ");
+					double deposit=sc.nextDouble();
+					b.deposit_Money(deposit);break;
+			case 3:System.out.println("Enter Money to be Withdraw : ");
+					double w=sc.nextDouble();
+					b.withdraw_Money(w);break;
+					
+			case 4: b.balance();break;
+			
+			case 5:System.exit(0);
+			
+			default: System.out.println("Invalid Choice=");
+		}
+						
 		}while(true);
 		
-		
-
 	}
 
 }
