a57d0350b6f3b20e33141dac8f26532dae5dead6
deveshagale@gmail.com <deveshagale@gmail.com>
2025-02-05 18:28:01 +0530

    Main File

--- src/Bank_Account_Management_System/Bank_Account_Management_System_Main.java
+++ src/Bank_Account_Management_System/Bank_Account_Management_System_Main.java
@@ -1,9 +1,25 @@
 package Bank_Account_Management_System;
 
+import java.util.Scanner;
+
 public class Bank_Account_Management_System_Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		
+		do {
+			
+			System.out.println("================================================");
+			System.out.println("Select the Option ");
+			System.out.println("1. Create a Bank Account");
+			System.out.println("2. Deposit Money ");
+			System.out.println("3. Withdraw Money ");
+			System.out.println("4. Check Balance ");
+			System.out.println("================================================");
+			
+		}while(true);
+		
+		
 
 	}
 
